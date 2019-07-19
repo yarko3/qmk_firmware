486545f91223f903f2541569ac9266321d3ee1f7
Yaroslav Senyuta <yarko3@hotmail.com>
2019-07-18 21:35:49 -0400

    ergodox: fix layer definition

--- keyboards/ergodox_ez/keymaps/yarko/keymap.c
+++ keyboards/ergodox_ez/keymaps/yarko/keymap.c
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                               |      |      |      |           |      |      |      |
  *                               `--------------------'           `--------------------'
  */
-[MDIA] = LAYOUT_ergodox(
+[MOUS] = LAYOUT_ergodox(
   // left hand
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
