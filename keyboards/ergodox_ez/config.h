3307756ec377c4abf4505798d5fdf8a7783d58cb
Yaroslav Senyuta <ysenyuta@google.com>
2019-09-26 09:12:52 -0400

    ergodox: update tap term again

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -55,7 +55,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
 
-#define TAPPING_TERM    123
+#define TAPPING_TERM    122
 #define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
 #define RETRO_TAPPING // sends the tap scancode when nothing else was pressed within the TAPPING_TERM
 
