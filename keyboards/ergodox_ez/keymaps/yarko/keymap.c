7a51fe38ead4827329f8b77e1bcdcb23092e8e6b
Yaroslav Senyuta <ysenyuta@google.com>
2020-01-01 13:26:27 -0500

    remove ctrl+a mapping from ergodox

--- keyboards/ergodox_ez/keymaps/yarko/keymap.c
+++ keyboards/ergodox_ez/keymaps/yarko/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                ,------|------|------|           |--------+------+------.
  *                                |      |      | LGui |           |  RGui  |      |      |
  *                                | Space| Alt  |------|           |--------| Bspc |Enter |
- *                                |      |      | MOUS |           | Ctrl+a |      |      |
+ *                                |      |      | MOUS |           |        |      |      |
  *                                `--------------------'           `----------------------'
  */
 [BASE] = LAYOUT_ergodox(
@@ -40,14 +40,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   KC_SPC,         KC_LALT, MO(MOUS),
 
   // right hand
-  KC_NO,      KC_6,    KC_7,   KC_8,    KC_9,   KC_0,    KC_DEL,
-  KC_RPRN,    KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,    KC_BSLS,
-              KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT,
-  KC_RBRC,    KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                       KC_EQL, KC_PLUS, KC_NO,  KC_NO,   KC_NO,
-  KC_NO,      KC_NO,
+  KC_NO,   KC_6,    KC_7,   KC_8,    KC_9,   KC_0,    KC_DEL,
+  KC_RPRN, KC_Y,    KC_U,   KC_I,    KC_O,   KC_P,    KC_BSLS,
+           KC_H,    KC_J,   KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+  KC_RBRC, KC_N,    KC_M,   KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+                    KC_EQL, KC_PLUS, KC_NO,  KC_NO,   KC_NO,
+  KC_NO,   KC_NO,
   KC_RGUI,
-  LCTL(KC_A), KC_BSPC, KC_ENT
+  KC_NO,   KC_BSPC, KC_ENT
 ),
 /* Media Keymap
  *
