07d169652e0881d632cf4456e137c3980dc0b121
Yaroslav Senyuta <ysenyuta@google.com>
2020-02-24 14:35:36 -0500

    ergodox: decrease time to max

--- keyboards/ergodox_ez/config.h
+++ keyboards/ergodox_ez/config.h
@@ -44,7 +44,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define MOUSEKEY_INTERVAL       20
 #define MOUSEKEY_DELAY          0
-#define MOUSEKEY_TIME_TO_MAX    20
+#define MOUSEKEY_TIME_TO_MAX    10
 #define MOUSEKEY_MAX_SPEED      7
 #define MOUSEKEY_WHEEL_DELAY 0
 
